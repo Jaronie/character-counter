ea06920f273e25728c421d89fdf2c5abacedbf54
jaronie <[200~Jisaa@BALONEY MINGW64 ~$ git config --global user.name jaronieJisaa@BALONEY MINGW64 ~$ git config --global user.email>
2026-04-16 11:37:08 -0700

    Tests made for counter and rel. freq

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,29 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
+
+    @Test
+    public void testCharacterCountFreq(){
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+        //Act
+        int actual = counter.getFrequency('l');
+
+
+        //Assert
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void testGetRelativeFreqMissingChar(){
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+
+        //Act
+        double actual = counter.getRelativePercentage('x');
+
+        //Assert
+        assertEquals(0.0, actual, 0.001);
+    }
     
 }
